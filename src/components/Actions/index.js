fee3a46c13ebf244930620a322cf80967bfbf94a
Henrique Silva de Freitas <69264667+hsfreitas92@users.noreply.github.com>
2022-06-18 10:46:15 -0300

    add input entrada/saída

--- src/components/Actions/index.js
+++ src/components/Actions/index.js
@@ -1,57 +1,57 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, TextInput } from "react-native";
 
-import { FolderPlus, Tag, CreditCard, AlignJustify, Settings } from "react-native-feather";
+import { FolderPlus, Tag, FolderMinus, Settings } from "react-native-feather";
 
 export default function Actions() {
   return (
-    <ScrollView style={styles.container} horizontal={true} showsHorizontalScrollIndicator={false}>
-      <TouchableOpacity style={styles.actionButton}>
-        <View style={styles.areaButton}>
-          <FolderPlus color="#000" width={26} height={26} />
-        </View>
-        <Text style={styles.labelButton}>Entradas</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <TouchableOpacity style={styles.actionButton}>
+          <View style={styles.areaButton}>
+            <FolderPlus color="#000" width={26} height={26} />
+          </View>
+          <Text style={styles.labelButton}>Entradas</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity style={styles.actionButton}>
-        <View style={styles.areaButton}>
-          <Tag color="#000" width={26} height={26} />
-        </View>
-        <Text style={styles.labelButton}>Compras</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.actionButton}>
+          <View style={styles.areaButton}>
+            <FolderMinus color="#000" width={26} height={26} />
+          </View>
+          <Text style={styles.labelButton}>Saídas</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity style={styles.actionButton}>
-        <View style={styles.areaButton}>
-          <CreditCard color="#000" width={26} height={26} />
-        </View>
-        <Text style={styles.labelButton}>Carteira</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.actionButton}>
+          <View style={styles.areaButton}>
+            <Settings color="#000" width={26} height={26} />
+          </View>
+          <Text style={styles.labelButton}>Conta</Text>
+        </TouchableOpacity>
+      </View>
 
-      <TouchableOpacity style={styles.actionButton}>
-        <View style={styles.areaButton}>
-          <AlignJustify color="#000" width={26} height={26} />
-        </View>
-        <Text style={styles.labelButton}>Boletos</Text>
-      </TouchableOpacity>
+      <View style={styles.areaFormulario}>
+        <Text style={styles.textoNome}>Entrada/Saida:</Text>
+        <TextInput style={styles.input}
+          placeholder="Valor"
+          maxLength={9}
+        />
+      </View>
 
-      <TouchableOpacity style={styles.actionButton}>
-        <View style={styles.areaButton}>
-          <Settings color="#000" width={26} height={26} />
-        </View>
-        <Text style={styles.labelButton}>Conta</Text>
-      </TouchableOpacity>
+    </View>
 
-    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    maxHeight: 84,
+    //maxHeight: 84,
     marginBottom: 14,
     marginTop: 18,
     paddingEnd: 14,
-    paddingStart: 14
+    paddingStart: 14,
+  },
+  content: {
+    flexDirection: 'row'
   },
   actionButton: {
     alignItems: 'center',
@@ -69,5 +69,28 @@ const styles = StyleSheet.create({
     marginTop: 4,
     textAlign: 'center',
     fontWeight: 'bold'
-  }
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderRadius: 4,
+    borderColor: '#999999',
+    backgroundColor: '#EEEEEE',
+    color: '#000000',
+    height: 38,
+    width: 95,
+    padding: 10,
+    marginBottom: 5,
+    marginTop: 5,
+    textAlign: 'center'
+  },
+  textoNome: {
+    fontSize: 17,
+    color: '#000000',
+    fontWeight: 'bold',
+  },
+  areaFormulario: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
 })
